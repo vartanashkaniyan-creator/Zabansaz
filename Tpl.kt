457d0a74ff1665e04ceef8d41545eb7326aa0567
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-26 17:20:20 +0330

    Update Tpl.kt
    Signed-off-by: vartanashkaniyan-creator <vartanashkaniyan@gmail.com>

--- Tpl.kt
+++ Tpl.kt
@@ -1,1 +1,17 @@
+data class Lesson(
+    val id: String,
+    val title: String,
+    val level: Int,
+    val items: List<Item>
+)
 
+data class Item(
+    val type: ItemType,
+    val q: String,
+    val a: String,
+    val extra: String? = null
+)
+
+enum class ItemType {
+    WORD, GRAMMAR, MCQ, FILL, LISTEN
+}
