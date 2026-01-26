c6278c186b2445100e7a6eec4e5f4d23da9e9624
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-26 19:38:04 +0330

    Update Gam.kt
    Signed-off-by: vartanashkaniyan-creator <vartanashkaniyan@gmail.com>

--- Gam.kt
+++ Gam.kt
@@ -1,1 +1,15 @@
 
+object Gam {
+
+    fun xpFor(level: Int): Int {
+        return level * 10
+    }
+
+    fun badge(progress: Int): String {
+        return when {
+            progress >= 100 -> "Gold"
+            progress >= 50 -> "Silver"
+            else -> "Bronze"
+        }
+    }
+}
