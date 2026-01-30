deca32e0ec84b72f4360395ce4fab7f4af8d2c52
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-31 00:58:20 +0330

    Update core_event_bus.js
    Signed-off-by: vartanashkaniyan-creator <vartanashkaniyan@gmail.com>

--- core_event_bus.js
+++ core_event_bus.js
@@ -1,6 +1,1 @@
-const listeners = new Map();
-export const eventBus = {
-    on: (e, cb) => listeners.set(e, [...(listeners.get(e)||[]), cb]),
-    emit: (e, data) => listeners.get(e)?.forEach(cb => cb(data))
-};
-console.log('EventBus loaded');
+
